8251cd94433cae6003d93e024ccf972b37ce3d0b
pon79 <pon79job@gmail.com>
2022-07-06 18:30:07 +0300

    add google test

--- test_version.cpp
+++ test_version.cpp
@@ -2,13 +2,9 @@
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() > 0);
+TEST(AppTest, VersionTest) {
+    ASSERT_TRUE(version() > 2) << "wrong version number";
 }
 
-BOOST_AUTO_TEST_SUITE_END()
